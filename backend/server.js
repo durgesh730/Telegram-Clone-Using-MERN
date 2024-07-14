b7209d6ea4ee55c15b4b36ced919ce674bcfc433
durgesh730 <durgesh.unicoglobal@gmail.com>
2024-07-14 12:26:18 +0530

    add deleted package file

--- backend/server.js
+++ backend/server.js
@@ -14,11 +14,11 @@ mongoose
   .catch((error) => console.log("An error occured..."));
 
 // Serve client folder
-app.use(express.static(path.join(__dirname, "client", "build")));
+// app.use(express.static(path.join(__dirname, "client", "build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+// });
 
 //   Listen to port
 exports.expressServer = app.listen(process.env.PORT || 4000, () =>
